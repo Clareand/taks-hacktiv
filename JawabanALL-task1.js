bd186b71b62194a4177fe9701ba06de53c9f8379
clareand <claraputrias@gmail.com>
2020-11-26 23:39:00 +0700

    Update JawabanALL-task1.js

--- JawabanALL-task1.js
+++ JawabanALL-task1.js
@@ -240,3 +240,50 @@ const videoData = [
 let presentTrue = videoData.filter((presented) => presented.present === true);
 console.log("---Jawaban Homework--- ");
 console.log(presentTrue.length)
+
+// practice I
+let table = [
+  ["Kode Absen", "Name", "No Hp", "Github"],
+  [
+    "RCTN043ONL004",
+    "Andika Wijaya",
+    "82316747626",
+    "https://github.com/andikawij",
+  ],
+  [
+    "RCTN043ONL005",
+    "Ken Grisya",
+    "081223123428",
+    "https://github.com/kengirisya",
+  ],
+  [
+    "RCTN043ONL002",
+    "Bagus Panghegar",
+    "082299304615",
+    "https://github.com/baguspanghegar",
+  ],
+  [
+    "RCTN043ONL001",
+    "M Genta Ari Shandi",
+    "085155476774",
+    "https://github.com/geardy47",
+  ],
+  [
+    "RCTN043ONL003",
+    "Nadya Sylviani",
+    "'081219101996",
+    "https://gitlab.com/nadyaslvn",
+  ],
+  [
+    "RCTN043ONL006",
+    "Mars Theolando",
+    "81511239138",
+    "https://github.com/Clareand",
+  ],
+  ["RCTN043ONL007", "Hapri Mariga", "082113958883", ""],
+  ["RCTN043ONL008", "Novia", "08980084400", ""],
+];
+
+let message = `Halo nama: ${table[6][1]}, kode absen: ${table[6][0]}, no hp: ${table[6][2]}, github link: ${table[6][3]} `;
+console.log('---Practice I---')
+console.log(message);
